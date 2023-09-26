7ce68d0ecd395e88911d9846c6c23c53645c99e1
fruiz <fruiz@LAPTOP-QBB4AB87>
2023-09-26 12:04:52 -0600

    Arreglo a TrabajadorHora

--- app/src/main/java/com/example/labtrabajadores/models/TrabajadorHora.java
+++ app/src/main/java/com/example/labtrabajadores/models/TrabajadorHora.java
@@ -38,7 +38,21 @@ public class TrabajadorHora extends Trabajador {
     }
 
     private void Calcular(){
-        super.totalPagar = this.numeroHoras*this.valorHora;
+        super.sueldoMensual = this.numeroHoras * this.valorHora;
+        float ri = super.sueldoMensual;
+        if(ri > 0 && ri <= 487.60){
+            super.descuentoISR = 0;
+        } else if(ri > 487.60 && ri <= 642.85){
+            super.descuentoISR = 17.48f + (super.sueldoMensual - 487.60f) * 0.10f;
+        } else if(ri > 642.85 && ri <= 915.82f){
+            super.descuentoISR = 32.70f + (super.sueldoMensual - 642.85f) * 0.10f;
+        } else if(ri > 915.82 && ri <= 2058.67){
+            super.descuentoISR = 60.08f + (super.sueldoMensual - 915.82f) * 0.20f;
+        } else{
+            super.descuentoISR = 288.57f + (super.sueldoMensual - 2058.67f) * 0.30f;
+        }
+        super.totalDescuentos = super.descuentoISR;
+        super.totalPagar = super.sueldoMensual-super.totalDescuentos;
     }
 
     @Override
